b1cc8f5e45cd1b8942eb935f97556fd4d73f8893
andrew101sanders <andrew101sanders@gmail.com>
2020-03-07 07:18:51 -0500

    updated file

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -2,18 +2,28 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-datafile_x = pd.get_dummies(pd.read_csv('adult.data',
-                                        usecols=['age', 'type_employer', 'fnlwgt', 'education', 'education_num',
-                                                 'marital',
-                                                 'occupation', 'relationship', 'race', 'sex', 'capital_gain',
-                                                 'capital_loss',
-                                                 'hr_per_week', 'country'],
-                                        skipinitialspace=True))
-datafile_y = pd.read_csv('adult.data',
-                         usecols=['income'],
-                         skipinitialspace=True)
-X_train, X_test, y_train, y_test = train_test_split(datafile_x, datafile_y.values.ravel(), test_size=0.2)
-Log_Reg = SGDClassifier(max_iter=2000, loss='log')
-Log_Reg.fit(X_train, y_train)
-print(Log_Reg.score(X_train, y_train))
-print(Log_Reg.score(X_test, y_test))
+datafile_x, datafile_y, X_train, X_test, y_train, y_test, Log_Reg = None, None, None, None, None, None, None
+
+
+def getData():
+    global datafile_x, datafile_y, X_train, X_test, y_train, y_test
+    datafile = pd.read_csv('adult.data', skipinitialspace=True)
+    datafile_x = pd.get_dummies(datafile.iloc[:, :-1])
+    datafile_y = datafile.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(datafile_x, datafile_y.values.ravel(), test_size=0.2)
+
+
+def trainModel():
+    global Log_Reg
+    Log_Reg = SGDClassifier(max_iter=2000, loss='log')
+    Log_Reg.fit(X_train, y_train)
+
+
+def getAccuracy():
+    print(Log_Reg.score(X_train, y_train))
+    print(Log_Reg.score(X_test, y_test))
+
+
+getData()
+trainModel()
+getAccuracy()
